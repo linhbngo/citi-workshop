55344b2a7db5ac32f9b44a10f34279119084f06a
Linh B Ngo <lngo@clemson.edu>
2020-04-10 10:59:14 -0400

    Update install_webserver.sh

--- install_webserver.sh
+++ install_webserver.sh
@@ -2,5 +2,5 @@
 set -x
 sudo apt update
 sudo apt install -y apache2
-sudo ufw allow in "Apache Full"'
-sudo systemctl status apache2'
+sudo ufw allow in "Apache Full"
+sudo systemctl status apache2
